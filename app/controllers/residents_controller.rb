79adf9311a94b0740cceb5c6c9dbaa4a92aecfc6
yuki07yuki <yuki07yuki@gmail.com>
2020-05-08 02:47:14 +0800

    Implement residents#new action

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -4,7 +4,18 @@ class ResidentsController < ApplicationController
   end
 
   def create
-    redirect_to '/residents/index'
+
+    @resident = Resident.new(resident_params)
+
+    if @resident.save
+      flash[:success] = "Resident succesfully registered"
+      redirect_to "/residents/index"
+      send_password_to_resident
+    else
+      flash.now[:danger] = 'Something went wrong!'
+      render 'new'
+    end
+
   end
 
   def index
@@ -19,5 +30,24 @@ class ResidentsController < ApplicationController
   def update
     redirect_to '/residents/index'
   end
+
+  private
+
+    def resident_params
+      add_password_to_params
+      params.require(:resident).permit(:floor, :unit, :name, :ic,
+        :phone, :email, :password, :password_confirmation)
+    end
+
+    def add_password_to_params
+      password = SecureRandom.urlsafe_base64
+      params[:resident][:password] = "000000"
+      params[:resident][:password_confirmation] = "000000"
+    end
+
+    def send_password_to_resident
+      # TODO: Implement password sending to the new registered resident
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
   get 'register', to: 'residents#new'
+  post 'register', to: 'residents#create'
+
   get 'residents/index'
 
   get 'residents/:floor/:unit/edit', to: 'residents#edit'
   patch 'residents/:floor/:unit/edit', to: 'residents#update'
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
