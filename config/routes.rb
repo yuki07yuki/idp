7f1862b5d25aabf7ffc8a2d73b22548c4770dc41
yuki07yuki <yuki07yuki@gmail.com>
2020-05-08 22:32:45 +0800

    Add Sessions#new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,27 @@
 Rails.application.routes.draw do
 
+# Resident
+  # register new resident
   get 'sessions/new'
   get 'register', to: 'residents#new'
   post 'register', to: 'residents#create'
 
+  # show all residents
   get 'residents/index'
 
+  # edit resident info
   get 'residents/:floor/:unit/edit', to: 'residents#edit'
   patch 'residents/:floor/:unit/edit', to: 'residents#update'
 
+  # delete resident entry
   delete 'residents/:floor/:unit', to: 'residents#destroy'
+
+
+# Sessions
+
+  # login
+  get 'login' , to: 'sessions#new'
+
+
 
 end
