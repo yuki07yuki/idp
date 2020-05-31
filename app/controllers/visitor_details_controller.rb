0d5071937c697121540e2721008301673c658c84
Yuki Yoshimine <yuki07yuki@gmail.com>
2020-06-01 06:30:12 +0800

    Add visitor_detail#edit action

--- app/controllers/visitor_details_controller.rb
+++ app/controllers/visitor_details_controller.rb
@@ -1,2 +1,17 @@
 class VisitorDetailsController < ApplicationController
+
+  def edit
+    # does the resident id exists
+    debugger
+    unless Resident.find_by(id: params[:resident_id])
+      # render invalid link
+      flash[:danger] = 'Invalid Link'
+      render 'invalid_link'
+      return
+    end
+
+    # valid token? && correct secret key?
+
+    # delete visitor_pass from the database
+  end
 end
