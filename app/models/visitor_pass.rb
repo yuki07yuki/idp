c5598511c9454aef128e4152318c6759ef73266b
Yuki Yoshimine <yuki07yuki@gmail.com>
2020-06-04 01:22:37 +0800

    Update visitor_pass.rb
    Include token validation

--- app/models/visitor_pass.rb
+++ app/models/visitor_pass.rb
@@ -1,9 +1,10 @@
 class VisitorPass < ApplicationRecord
 
   validates(:visitors_name,         presence: true, length: {maximum: 50})
-  validates(:resident_id,  presence: true, length: {maximum: 50})
+  validates(:resident_id,           presence: true, length: {maximum: 50})
   validates(:visitors_email,        presence: true, length: {maximum: 50})
-  validates(:secret_key,   presence: true, length: {maximum: 50})
+  validates(:token,                 presence: true, length: {maximum: 50})
+  validates(:secret_key,            presence: true, length: {maximum: 50})
 
 
 
