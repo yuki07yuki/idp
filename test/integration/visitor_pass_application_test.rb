b04622a2bdf2a0a93a386515e5ee3ddefc6b1b52
Yuki Yoshimine <yuki07yuki@gmail.com>
2020-06-04 02:44:41 +0800

    Update visitor_pass_application_test.rb

--- test/integration/visitor_pass_application_test.rb
+++ test/integration/visitor_pass_application_test.rb
@@ -13,25 +13,48 @@ class VisitorPassApplicationTest < ActionDispatch::IntegrationTest
     post visitor_passes_path , params: {resident_key: "" }
 
     assert_template 'visitor_passes/new'
-    assert_equal "Invalid resident key.", flash[:danger], "Wrong flash"
 
     flash_all_cleared?
+  end
+
+  test 'cannot apply if any of the field is empty' do
+
+    get new_visitor_pass_path
+    assert_template 'visitor_passes/new'
+
+    post visitor_passes_path , params: {resident_key: "000000",
+                                          visitors_email: "yuki07yuki@gmail.com",
+                                          visitors_name: "",
+                                          secret_key: "" }
+    assert_template 'visitor_passes/new'
+    assert_select 'div#error_explanation'
+
   end
 
   test 'can apply if you are a resident' do
     get new_visitor_pass_path
     assert_template 'visitor_passes/new'
-
-    post visitor_passes_path , params: {resident_key: "000000",
-                                          visitors_email: "yuki07yuki@gmail.com"}
-    # assert_equal "You are a resident", flash[:success], "Wrong flash"
-
-
+    before = VisitorPass.count
+    post visitor_passes_path , params: {  resident_key: "000000",
+                                          visitors_email: "yuki07yuki@gmail.com",
+                                          visitors_name: "athiga",
+                                          secret_key: "banana" }
+    after = VisitorPass.count
+    assert_equal before + 1 , after
+    assert_template 'home_pages/home'
+    assert_equal success_message, flash[:success]
+    flash_all_cleared?
   end
 
 
 
   private
+
+      def success_message
+        msg = "The email has been sent to the visitor.\n"
+        msg += "Thank you."
+        msg
+      end
 
 
 
