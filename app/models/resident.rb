13e82a13227c6aefdf262b844163dcf40a64b2bd
Yuki Yoshimine <yuki07yuki@gmail.com>
2020-06-06 00:56:32 +0800

    Update resident.rb

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -2,7 +2,8 @@ class Resident < ApplicationRecord
   has_secure_password
   validate :the_only_resident, on: :create
   validates :name, presence: true
-  validates :ic, presence: true
+  # validates :ic, presence: true
+  validate :ic_is_present
   validates :phone, presence: true
   validates :email, presence: true
   validates :password, presence: true , length: { minimum: 6 }, allow_nil: true
@@ -17,8 +18,13 @@ class Resident < ApplicationRecord
 
     def the_only_resident
       if Resident.find_by(floor: floor, unit: unit)
-        errors.add(:base, "Only one resident can be registered in a unit.")
+        errors.add(:base, "Only one resident can be registered per unit")
       end
     end
 
+    def ic_is_present
+      if ic.empty?
+        errors.add(:base, "IC can't be blank")
+      end
+    end
 end
