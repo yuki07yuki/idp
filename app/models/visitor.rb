9eba1813e1c89f9ea0c3d7c2dda5b23907d3be74
Yuki Yoshimine <yuki07yuki@gmail.com>
2020-06-10 01:23:20 +0800

    Update visitor.rb

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,7 +1,15 @@
 class Visitor < ApplicationRecord
   validates :name, presence: true
-  validates :ic, presence: true
+  validate  :ic_must_be_present
   validates :phone, presence: true
   validates :email, presence: true
   validates :secret_key, presence: true
+
+  private
+
+      def ic_must_be_present
+        if ic.empty?
+          errors.add(:base, "IC can't be blank")
+        end
+      end
 end
