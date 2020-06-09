bfecd74c2f906bbbfd03e505cb73484e5f01858b
Yuki Yoshimine <yuki07yuki@gmail.com>
2020-06-10 01:25:05 +0800

    Update visitor_registration_test.rb

--- test/integration/visitor_registration_test.rb
+++ test/integration/visitor_registration_test.rb
@@ -8,35 +8,103 @@ class VisitorRegistrationTest < ActionDispatch::IntegrationTest
 
   end
 
-  test 'cannot visit a page if id and token do not match' do
-    get new_visitor_path(resident_id: "1", token: @pass.token) # wrong id corrent token
+  test 'visitor cannot visit a page if id and token do not match' do
+    get new_visitor_path(resident_id: wrong_id , token: @pass.token) # wrong id corrent token
     assert_equal 'Invalid Link', flash[:danger]
-    assert_redirected_to root_path
-    follow_redirect!
+    assert_template 'home_pages/home'
     flash_cleared?
 
-    get new_visitor_path(resident_id: @resident.id, token: "123456") # corrent id wrong token
+    get new_visitor_path(resident_id: @resident.id, token: wrong_token ) # corrent id wrong token
     assert_equal 'Invalid Link', flash[:danger]
-    assert_redirected_to root_path
-    follow_redirect!
+    assert_template 'home_pages/home'
     flash_cleared?
 
+  end
 
-    get new_visitor_path(resident_id: @resident.id, token: "12345") # corrent id correct token
-    assert_equal "ok", flash[:ok]
+  test 'visitor cannot register if any field is empty' do
+    get new_visitor_path(resident_id: @resident.id, token: @pass.token )
+
+    before = Visitor.count
+    [:name, :ic, :phone, :email, :secret_key].each do |field|
+      submit_form( {field => ""} )
+      field = change_case field
+      assert_select 'ul',
+        {text: "#{field} can't be blank"},
+        "incorrect error msg for #{field}"
+      after = Visitor.count
+      assert_equal before, after, "Empty #{field} was allowed"
+    end
+
 
   end
 
-  test 'cannot register if any field is empty' do
+  test 'visitor can register without a car' do
+    before = Visitor.count
+    submit_form(car: "")
+    after = Visitor.count
 
+    assert_equal before + 1, after, "Visitor could not register without a car"
+
+    assert_equal 'The QR code has been sent to your email', flash[:success]
+    assert_template  'home_pages/home'
+
+    flash_cleared?
   end
 
-  test 'can register without a car' do
+  test 'visitor can register with a car' do
+    before = Visitor.count
+    submit_form
+    after = Visitor.count
 
+    assert_equal before + 1, after, "Visitor could not register"
+
+    assert_equal 'The QR code has been sent to your email', flash[:success]
+    assert_template  'home_pages/home'
+
+    flash_cleared?
   end
 
-  test 'can register with car' do
+  test 'visitor pass should be deleted after visitor registration' do
+    before = VisitorPass.count
+    submit_form
+    after = VisitorPass.count
 
+    assert_equal before - 1, after, "Visitor Pass was not deleted"
+
+    get new_visitor_path( resident_id: @resident.id, token: @pass.token )
+    assert_template 'home_pages/home'
+    assert_equal "Invalid Link", flash[:danger], "Wrong flash message"
+    flash_cleared?
   end
 
+
+  private
+
+      def submit_form(
+        name: "Athiga",      ic: "AB12345",
+        phone: "0123456789", email: "idp@example.com",
+        car: "12345",        secret_key: "banana" )
+
+        post visitors_path,
+          params: { visitor: {  name: name,
+                                ic:   ic,
+                                phone: phone,
+                                email: email,
+                                car: car,
+                                secret_key: secret_key }}
+      end
+
+
+      def change_case(field)
+        return "Secret key" if field == :secret_key
+        field == :ic ? field.to_s.upcase : field.to_s.capitalize
+      end
+
+      def wrong_id
+        "1"
+      end
+
+      def wrong_token
+        "123456789"
+      end
 end
