3d6071779f66a47715d07987a77d8f93f7fdabfe
Yuki Yoshimine <yuki07yuki@gmail.com>
2020-06-12 02:29:44 +0800

    Update resident_mailer.rb

--- app/mailers/resident_mailer.rb
+++ app/mailers/resident_mailer.rb
@@ -11,4 +11,9 @@ class ResidentMailer < ApplicationMailer
     mail to: @visitor_pass.visitors_email, subject: "Please Submit Your Details"
   end
 
+  def qrcode(visitor, qrcode)
+    @visitor = visitor
+    @qrcode = qrcode
+    mail to: @visitor.email, subject: "QR code for your visitor pass".capitalize
+  end
 end
